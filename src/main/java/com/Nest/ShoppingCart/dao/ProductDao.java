e2f8be814fab228cee56ea3ec8b7cabbbe09457b
Salooma <saloomaali2000@gmail.com>
2022-12-07 23:37:58 +0530

    search

--- src/main/java/com/Nest/ShoppingCart/dao/ProductDao.java
+++ src/main/java/com/Nest/ShoppingCart/dao/ProductDao.java
@@ -1,7 +1,13 @@
 package com.Nest.ShoppingCart.dao;
 
 import com.Nest.ShoppingCart.model.Products;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductDao extends CrudRepository<Products, Integer> {
+
+    @Query(value = "SELECT `id`, `category`, `description`, `image`, `name`, `price` FROM `products` WHERE `name` = :name", nativeQuery = true)
+    List<Products> searchProduct(String name);
 }
